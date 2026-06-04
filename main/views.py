9ef1e6b33a80fba86242d2f12ae29f75a98e47e7
shantiaparzade <shantiaparzade@gmail.com>
2026-06-04 18:45:34 +0330

    implement contact form submission

--- main/views.py
+++ main/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
 
 
 def index(request):
+
+    if request.method == 'POST':
+
+        Contact.objects.create(
+            name=request.POST.get('name'),
+            email=request.POST.get('email'),
+            phone=request.POST.get('phone'),
+            request_type=request.POST.get('request_type'),
+            message=request.POST.get('message')
+        )
+
+        return redirect('home')
+
     return render(request, 'index.html')
