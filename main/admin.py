99736b4bf3d79683ab0032d63f4a99020c77183e
shantiaparzade <shantiaparzade@gmail.com>
2026-06-04 16:25:47 +0330

    create contact model and admin configuration

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'phone',
+        'email',
+        'request_type',
+        'created_at'
+    )
+
+    search_fields = (
+        'name',
+        'phone',
+        'email'
+    )
+
+    list_filter = (
+        'request_type',
+        'created_at'
+    )
--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    REQUEST_TYPES = [
+        ('مشاوره رایگان', 'مشاوره رایگان'),
+        ('درخواست پروژه', 'درخواست پروژه'),
+        ('سایر موارد', 'سایر موارد'),
+    ]
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    request_type = models.CharField(max_length=50, choices=REQUEST_TYPES)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
